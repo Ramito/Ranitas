f22560f97a483aead2427abffdc8fb6d189b1acc
Ramito <elramito@gmail.com>
2017-10-17 00:08:19 -0700

    FlyData!

---
+++ RanitasData/FlyData.cs
@@ -0,0 +1,17 @@
+﻿namespace Ranitas.Data
+{
+    public sealed class FlyData
+    {
+        public int MaxActiveFlies = 100;
+        public float FliesPerSecond = 0.5f;
+
+        public float Width;
+        public float Height;
+
+        public float MinHeight = 25f;
+        public float MaxHeight = 400f;
+
+        public float MinSpeed = 10f;
+        public float MaxSpeed = 600f;
+    }
+}
