25ec15dab72db22bb9c1f5400af3144b92522813
Ramito <elramito@gmail.com>
2017-10-19 00:02:30 -0700

    Added a clamp01 method!

---
+++ Ranitas/Core/MathExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Ranitas.Core
+{
+    public static class MathExtensions
+    {
+        public static float Clamp01(float value)
+        {
+            if (value >= 1f)
+            {
+                return 1f;
+            }
+            if (value <= 0f)
+            {
+                return 0f;
+            }
+            return value;
+        }
+    }
+}
