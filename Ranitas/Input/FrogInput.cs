2fefb53698feee8d0f0ef1f84badf90f8561d38f
Ramito <elramito@gmail.com>
2017-10-21 00:31:37 -0700

    New FrogInput class and processor

---
+++ Ranitas/Input/FrogInput.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+
+namespace Ranitas.Input
+{
+    [Flags]
+    public enum FrogSignals
+    {
+        None = 0,
+        Jump = 1 << 0,
+        Toungue = 1 << 1,
+    }
+
+    public struct FrogInput
+    {
+        public readonly Vector2 NormalizedDirection;
+        public readonly float Magnitude;
+        public readonly FrogSignals Signals;
+
+        public FrogInput(Vector2 normalizedDirection, float magnitude, FrogSignals signals)
+        {
+            NormalizedDirection = normalizedDirection;
+            Magnitude = magnitude;
+            Signals = signals;
+        }
+    }
+}
---
+++ Ranitas/Input/InputProcessor.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Ranitas.Core;
+
+namespace Ranitas.Input
+{
+    public sealed class InputProcessor
+    {
+        public const float kMinMagnitude = 0.15f;
+        public const float kMaxMagnitude = 0.85f;
+        public const float kRangeNormalizer = 1f / (kMaxMagnitude - kMinMagnitude);
+
+        public readonly FrogInput[] Inputs;
+
+        public InputProcessor(int playerCount)
+        {
+            Inputs = new FrogInput[playerCount];
+        }
+
+        public void ProcessInput(int playerIndex)
+        {
+            GamePadState state = GamePad.GetState(playerIndex);
+
+            float scaledMagnitude = 0f;
+            Vector2 direction = state.ThumbSticks.Left;
+            float rawMagnitude = direction.Length();
+            if (rawMagnitude >= kMinMagnitude)
+            {
+                scaledMagnitude = MathExtensions.Clamp01((rawMagnitude - kMinMagnitude) * kRangeNormalizer);
+                direction.Normalize();
+            }
+            else
+            {
+                direction = Vector2.Zero;
+            }
+
+            FrogSignals signal = FrogSignals.None;
+            if (state.IsButtonDown(Buttons.A))
+            {
+                signal = signal | FrogSignals.Jump;
+            }
+            if (state.IsButtonDown(Buttons.X))
+            {
+                signal = signal | FrogSignals.Toungue;
+            }
+
+            Inputs[playerIndex] = new FrogInput(direction, scaledMagnitude, signal);
+        }
+
+        public FrogInput GetFrogInput(int playerIndex)
+        {
+            return Inputs[playerIndex];
+        }
+    }
+}
