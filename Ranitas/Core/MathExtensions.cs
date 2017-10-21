c7b4fe4b25b5fcd08e67270196b87f00426faeae
Ramito <elramito@gmail.com>
2017-10-21 00:30:38 -0700

    Simple tweak to Clamp01

--- Ranitas/Core/MathExtensions.cs
+++ Ranitas/Core/MathExtensions.cs
@@ -1,18 +1,12 @@
-﻿namespace Ranitas.Core
+﻿using Microsoft.Xna.Framework;
+
+namespace Ranitas.Core
 {
     public static class MathExtensions
     {
         public static float Clamp01(float value)
         {
-            if (value >= 1f)
-            {
-                return 1f;
-            }
-            if (value <= 0f)
-            {
-                return 0f;
-            }
-            return value;
+            return MathHelper.Clamp(value, 0f, 1f);
         }
     }
 }
