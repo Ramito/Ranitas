22dce5b69c2b591b989301aff7e95df85aa6fd7f
Ramito <elramito@gmail.com>
2018-08-31 23:39:01 -0700

    Experimental: Use reflection to create slices by taking a host structure with requirements. WIP!
    Still figuring out all the steps, but I manage to find and instantiate
    the value registries!
    Next step is to bind them.

---
+++ Ranitas/Core/ECS/EntityRegistryExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+
+namespace Ranitas.Core.ECS
+{
+    public static class EntityRegistryExtensions
+    {
+        //TODO: Maybe this should return the created slice rather than take a ref
+        public static void MakeMagicSlice<TSlice>(this EntityRegistry registry, ref TSlice slice)
+        {
+            object boxedSlice = slice;
+            Type sliceType = typeof(TSlice);
+
+            var sliceConfiguration = registry.ConfigureSlice();
+
+            FieldInfo[] memberFields = sliceType.GetFields();
+            foreach (FieldInfo field in memberFields)
+            {
+                Type fieldType = field.FieldType;
+                if (fieldType.Name == typeof(ValueRegistry<>).Name)
+                {
+                    object fieldInstance = Activator.CreateInstance(fieldType, new object[] { registry.Capacity });
+                    //Call require on the slice, but then we need to be able to invoke with the right generic parameter!
+                    field.SetValue(boxedSlice, fieldInstance);
+                }
+
+            }
+            slice = (TSlice)boxedSlice;
+        }
+    }
+}
