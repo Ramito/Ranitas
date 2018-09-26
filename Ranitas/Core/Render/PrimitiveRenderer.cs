37b5d9ec9c6b9c9c4f811d355a04b56b69cd0ed7
Ramito <elramito@gmail.com>
2018-09-25 21:05:25 -0700

    Tweaks to the primitive renderer

--- Ranitas/Core/Render/PrimitiveRenderer.cs
+++ Ranitas/Core/Render/PrimitiveRenderer.cs
@@ -21,6 +21,8 @@ namespace Ranitas.Core.Render
         {
             if (mCurrentIndex > 0)
             {
+                Device.SetVertexBuffer(mVertexBuffer);
+                Device.DepthStencilState = DepthStencilState.DepthRead;
                 mVertexBuffer.SetData(mVertexBufferData, 0, mCurrentIndex);
                 int triangleCount = mCurrentIndex - 2;
                 Device.DrawPrimitives(PrimitiveType.TriangleStrip, 0, triangleCount);
@@ -30,12 +32,14 @@ namespace Ranitas.Core.Render
 
         public void PushRect(Rect rect, Color color)
         {
-            mVertexBufferData[mCurrentIndex].Position = new Vector3(rect.MaxCorner, 0f);
-            mVertexBufferData[mCurrentIndex + 1].Position = new Vector3(rect.MaxCorner, 0f);
-            mVertexBufferData[mCurrentIndex + 2].Position = new Vector3(rect.MaxMinCorner, 0f);
-            mVertexBufferData[mCurrentIndex + 3].Position = new Vector3(rect.MinMaxCorner, 0f);
-            mVertexBufferData[mCurrentIndex + 4].Position = new Vector3(rect.MinCorner, 0f);
-            mVertexBufferData[mCurrentIndex + 5].Position = new Vector3(rect.MinCorner, 0f);
+            const float kDepth = 0f;
+
+            mVertexBufferData[mCurrentIndex].Position = new Vector3(rect.MaxCorner, kDepth);
+            mVertexBufferData[mCurrentIndex + 1].Position = new Vector3(rect.MaxCorner, kDepth);
+            mVertexBufferData[mCurrentIndex + 2].Position = new Vector3(rect.MaxMinCorner, kDepth);
+            mVertexBufferData[mCurrentIndex + 3].Position = new Vector3(rect.MinMaxCorner, kDepth);
+            mVertexBufferData[mCurrentIndex + 4].Position = new Vector3(rect.MinCorner, kDepth);
+            mVertexBufferData[mCurrentIndex + 5].Position = new Vector3(rect.MinCorner, kDepth);
 
             mVertexBufferData[mCurrentIndex].Color = color;
             mVertexBufferData[mCurrentIndex + 1].Color = color;
@@ -51,7 +55,6 @@ namespace Ranitas.Core.Render
         {
             const int kVertexCount = 250 * 6;
             mVertexBuffer = new VertexBuffer(Device, typeof(VertexPositionColor), kVertexCount, BufferUsage.WriteOnly);
-            Device.SetVertexBuffer(mVertexBuffer);
             mVertexBufferData = new VertexPositionColor[kVertexCount];
             mCurrentIndex = 0;
         }
